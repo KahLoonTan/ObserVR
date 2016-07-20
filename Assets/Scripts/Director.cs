f68eb025a3f98553a8625f3adba79e50b309a73a
Muhammad Asyraf bin Ibrahim <asyzruffz@gmail.com>
2016-07-20 22:40:02 +0800

    Added a Sci-fi level
    NPC can run araund level

--- Assets/Scripts/Director.cs
+++ Assets/Scripts/Director.cs
@@ -4,18 +4,20 @@ using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Director : MonoBehaviour {
 
-    public AICharacterControl character;
+    public GameObject character;
     public Transform target1;
     public Transform target2;
     public Transform target3;
     public Transform target4;
 
+    public AICharacterControl ai;
     private int currentTarget = -1;
 
     // Use this for initialization
     void Start () {
-	
-	}
+        ai = character.GetComponent<AICharacterControl>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -26,18 +28,18 @@ public class Director : MonoBehaviour {
             switch (currentTarget)
             {
                 case 0:
-                    character.target = target1;
+                    ai.target = target1;
                     break;
                 case 1:
-                    character.target = target2;
+                    ai.target = target2;
                     break;
                 case 2:
-                    character.target = target3;
+                    ai.target = target3;
                     break;
                 case 3:
-                    character.target = target4;
+                    ai.target = target4;
                     break;
             }
         }
-	}
+    }
 }
